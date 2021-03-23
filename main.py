fe0c8337606d4a9aa207a15e366172d949272197
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-03-23 15:29:18 +0530

    db created

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///urls.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+
+
+class Urls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String, unique=False, nullable=False)
+    keyword = db.Column(db.String, unique=True, nullable=False)
 
 
 @app.route('/', methods=['post', 'get'])
 def index():
     if request.method == "POST":
-        url=request.form.get("url")
-        keyword=request.form.get("keyword")
+        url = request.form.get("url")
+        keyword = request.form.get("keyword")
     return render_template('index.html')
 
 
